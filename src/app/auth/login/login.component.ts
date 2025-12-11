ca908abb3a955bae6bc3b8c9fad3c2128f5a26f3
kaduportugal@gmail.com <cportugal@meucashcard.com.br>
2025-12-11 17:52:22 -0300

    ajustes no login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,47 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { LoginService } from 'src/app/service/administrator/login.service';
-import {NgxSpinnerService} from "ngx-spinner";
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   standalone: false,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  hide: boolean = true;
-  isLoading!: boolean;
+  isLoading = false;
 
-  public username: String = ''
-  public password: String = ''
+  public username: string = '';
+  public password: string = '';
 
-  public formEstado: string = 'disabled'
+  passwordVisible = false;
 
   constructor(
     private loginService: LoginService,
-    private spinner: NgxSpinnerService
-  ) { }
+    private spinner: NgxSpinnerService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = false;
-    this.loginService.logado()
+    this.loginService.logado();
   }
 
-  togglePasswordVisibility() {
-    this.hide = !this.hide;
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
   }
 
   public async confirmaLogin(): Promise<void> {
+    if (this.isLoading) {
+      return;
+    }
+
     this.isLoading = true;
+    this.cdr.detectChanges();
     this.spinner.show();
 
-    var userApi = Object.assign({
-      user_login: this.username,
-      access_key: this.password
-    });
-    await this.loginService.efetivaLogin(userApi);
+    try {
+      const userApi = {
+        user_login: this.username,
+        access_key: this.password,
+      };
 
-    this.isLoading = false;
-    this.spinner.hide();
+      // Se quiser testar visualmente:
+      await new Promise((res) => setTimeout(res, 2000));
+
+      await this.loginService.efetivaLogin(userApi);
+    } finally {
+      this.isLoading = false;
+      console.log('isLoading no finally:', this.isLoading);
+      this.spinner.hide();
+      this.cdr.detectChanges();
+    }
   }
 }
